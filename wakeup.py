d4dd5409859f227783b00e3dc1e652e12b741281
lockedleer <Daniel.Tyler.Brennan@gmail.com>
2020-08-05 08:55:31 -0400

    Finish infinite loop for script game based on daily routine of covid19 lockdown.

--- wakeup.py
+++ wakeup.py
@@ -1,11 +1,13 @@
+import random
+
 def decision_options(option1, option2):
     print("(%r or %r)" % (option1, option2))
 
 def begin_day():
     sleep_in_counter = 0
     print("Do you want to get the day started?")
-    decision_options("Out of bed", "Stay in bed")
     while True:
+        decision_options("Out of bed", "Stay in bed")
         decision = input("> ")
         if "stay" in decision and sleep_in_counter == 2:
             print("Clearly you don't want to go to work today.\nCall in sick and wake up another day.")
@@ -45,8 +47,8 @@ def morning_routine():
 
 def daily_activity():
     print("Now that you're ready, how would you like to spend your day?")
-    decision_options("Work", "Outside")
     while True:
+        decision_options("Work", "Outside")
         decision = input("> ")
         if "work" in decision:
             work_time_skip()
@@ -73,17 +75,53 @@ def work_time_skip():
 
 def walk_time_skip():
     print("You decide to go for a long walk today.\nDo you want to go East or West")
-    decision_options("East", "West")
+    outdoor_direction_decision()
 
 def bike_time_skip():
     print("You decide to go for a long bike ride today.\nDo you want to go East or West")
-    decision_options("East", "West")
+    outdoor_direction_decision()
+
+
+def outdoor_direction_decision():
+    random_occurences = [
+    'You see a homeless man, and search your pocket for change. You give him what little you can and he smiles.',
+    'A dog is running wildly without supervision, you wonder where it\'s owner is.',
+    'There\'s a family enjoying a picnic in a park, they look content and happy in life.',
+    'There\'s a protest occuring, you wonder what it is for in this desolate time. It could be anything.',
+    'A busker is playing your favorite song. You sit and listen for a bit before giving them some pocket change.'
+    ]
+    while True:
+        decision_options("East", "West")
+        decision = input("> ")
+        if "east" in decision or "west" in decision:
+            print("You head off to the %r, Along the way you see:" % decision)
+            for i in range(3):
+                print(random.choice(random_occurences))
+            afternoon_routine()
+        else:
+            print("Make a decision.")
+
 
 def afternoon_routine():
     print("It's the afternoon, and you're free to enjoy your hobbies.")
+    while True:
+        decision_options("Art", "Video Games")
+        decision = input("> ")
+        if "art" in decision:
+            print("You decide to paint and draw. \nThe Warhammer figures that have been left unpainted for months now are beckoning.")
+            nighttime_routine()
+        elif "games" in decision:
+            print("Video Games and adventure are calling, you settle in for the long haul with snacks and beverages.")
+            nighttime_routine()
+        else:
+            print("Make a decision.")
 
-    
-
+def nighttime_routine():
+    print("After a long day, it's time for bed.")
+    print("Taking the time to brush your teeth, and wash your face,\nyou make your way to bed and read until you pass out.")
+    print("During lockdown everyday has been the same routine, with subtle variety thrown in. \nMost days have been good, but some are long and unbearable.")
+    print("Oh well! there's always tomorrow. Time to start another day.\n\n")
+    start()
 
 def start():
     while True:
